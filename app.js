c8b634c99210e3dd7612ea02589327b013e4c7fc
themarcosf <marcosaurelio.adm@gmail.com>
2022-11-10 12:01:10 -0300

    express setup, routes

--- app.js
+++ app.js
@@ -1,17 +1,50 @@
-const app = require("express")();
+// core modules
+const express = require("express");
+const fs = require("fs");
+////////////////////////////////////////////////////////////////////////
 
-app.get("/", (req, res) =>
+// data load
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, {
+    encoding: "utf-8",
+  })
+);
+////////////////////////////////////////////////////////////////////////
+
+// SERVER
+const app = express();
+
+// middlewares
+app.use(express.json()); //adds body to request
+
+// routing [JSEND standard]
+app.get("/api/v1/tours", (req, res) =>
   res
     .status(200)
-    .json({ status: "Sucess", data: { type: "GET Request", app: "Natours" } })
+    .json({
+      status: "success",
+      results: tours.length,
+      data: { tours },
+    })
     .end()
 );
 
-app.post("/", (req, res) =>
-  res
-    .status(200)
-    .json({ status: "Success", data: { type: "POST Request", app: "Natours" } })
-    .end()
-);
+app.post("/api/v1/tours", (req, res) => {
+  const _id = tours.at(-1).id + 1;
+  const _tour = Object.assign({ id: _id }, req.body);
+  tours.push(_tour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) =>
+      res
+        .status(201)
+        .json({
+          status: "Success",
+          data: { tour: _tour },
+        })
+        .end()
+  );
+});
 
 app.listen(8000, "127.0.0.1", () => console.log("Server running on port 8000"));
