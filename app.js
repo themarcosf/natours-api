65dd864b3c5442f8320774e745e42d97c964f9d3
themarcosf <marcosaurelio.adm@gmail.com>
2022-11-11 20:35:35 -0300

    environment variables

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ const app = express();
 
 // middlewares
 app.use(express.json()); // adds body to request
-app.use(morgan("dev")); // http requests logger
 app.use(express.static(`${__dirname}/public`)); //serves static files
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
+if (process.env.NODE_ENV == "development") app.use(morgan("dev")); // http requests logger
 
 // routers
 app.use("/api/v1/tours", tourRouter);
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
+// environment variables
+require("dotenv").config({ path: "./config.env" });
+
+// server
 const app = require("./app");
 
-const localhost = "127.0.0.1";
-const port = 8000;
-
-// server start
-app.listen(port, localhost, () =>
-  console.log(`Server running on port ${port}`)
+app.listen(process.env.PORT || 8000, process.env.HOST || "127.0.0.1", () =>
+  console.log(`Server running on port ${process.env.PORT}`)
 );
