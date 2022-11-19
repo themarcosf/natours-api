4100e61b380fe7d0d98296f845f0b64237d43efa
themarcosf <marcosaurelio.adm@gmail.com>
2022-11-18 23:34:09 -0300

    manage data using cli - argv prompt

---
+++ dev-data/data/cli-data-mngmt.js
@@ -0,0 +1,56 @@
+/*
+command line script for importing and deleting collections of data
+*/
+
+// required modules
+const dotenv = require("dotenv").config({
+  path: `${__dirname}/../../config.env`,
+});
+const fs = require("fs");
+const mongoose = require("mongoose");
+const Tour = require("./../../models/tourModel");
+////////////////////////////////////////////////////////////////////////////////
+
+// database
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB)
+  .then((conn) => console.log(`DB Connected: ${conn.connections[0].name}`));
+////////////////////////////////////////////////////////////////////////////////
+
+// data management
+const data = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, "utf-8")
+);
+
+const _import = async () => {
+  try {
+    await Tour.create(data);
+    console.log("Data successfully loaded");
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+const _delete = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log("Data successfully deleted");
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv.includes("--import")) {
+  _import();
+}
+
+if (process.argv.includes("--delete")) {
+  _delete();
+}
