af704019f47576bf93cd2c080899d637a6349fd2
themarcosf <marcosaurelio.adm@gmail.com>
2022-11-18 22:19:53 -0300

    better Tour model

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
-if (process.env.NODE_ENV == "development") app.use(morgan("dev")); // http requests logger
+if (process.env.NODE_ENV === "development") app.use(morgan("dev")); // http requests logger
 
 // routers
 app.use("/api/v1/tours", tourRouter);
--- models/tourModel.js
+++ models/tourModel.js
@@ -1,18 +1,62 @@
 const mongoose = require("mongoose");
 
+// mongoose format: BSON
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, "Tour name is required."],
+    required: [true, "name is required."],
+    trim: true,
     unique: true,
+  },
+  duration: {
+    type: Number,
+    required: [true, "duration is required"],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, "maxGroupSize is required"],
+  },
+  difficulty: {
+    type: String,
+    required: [true, "difficulty is required"],
+    trim: true,
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+    trim: true,
   },
   price: {
     type: Number,
-    required: [true, "Tour price is required."],
+    required: [true, "price is required."],
   },
-  rating: {
-    type: Number,
-    default: 4.5,
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    required: [true, "summary is required"],
+    trim: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+    required: [true, "description is required"],
+  },
+  imageCover: {
+    type: String,
+    required: [true, "imageCover is required"],
+  },
+  images: [String],
+  startDates: {
+    type: [Date],
+    required: [true, "startDates is required"],
+  },
+  createdAd: {
+    type: Date,
+    default: Date.now(),
   },
 });
 
