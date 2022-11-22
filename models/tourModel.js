e9e4a59f780c7aefde68a455b9f9acd1b2c77214
themarcosf <marcosaurelio.adm@gmail.com>
2022-11-22 04:09:39 -0300

    virtual properties

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,70 +1,82 @@
-/*
-EXPLANATION : select: false
-
-used to permanently hide sensitive data from clients
-*/
-
 const mongoose = require("mongoose");
 
 // mongoose format: BSON
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "name is required."],
-    trim: true,
-    unique: true,
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "name is required."],
+      trim: true,
+      unique: true,
+    },
+    duration: {
+      type: Number,
+      required: [true, "duration is required"],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, "maxGroupSize is required"],
+    },
+    difficulty: {
+      type: String,
+      required: [true, "difficulty is required"],
+      trim: true,
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: [true, "price is required."],
+    },
+    priceDiscount: Number,
+    summary: {
+      type: String,
+      required: [true, "summary is required"],
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+      required: [true, "description is required"],
+    },
+    imageCover: {
+      type: String,
+      required: [true, "imageCover is required"],
+    },
+    images: [String],
+    startDates: {
+      type: [Date],
+      required: [true, "startDates is required"],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false, //used to permanently hide sensitive data from clients
+    },
   },
-  duration: {
-    type: Number,
-    required: [true, "duration is required"],
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, "maxGroupSize is required"],
-  },
-  difficulty: {
-    type: String,
-    required: [true, "difficulty is required"],
-    trim: true,
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 4.5,
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0,
-    trim: true,
-  },
-  price: {
-    type: Number,
-    required: [true, "price is required."],
-  },
-  priceDiscount: Number,
-  summary: {
-    type: String,
-    required: [true, "summary is required"],
-    trim: true,
-  },
-  description: {
-    type: String,
-    trim: true,
-    required: [true, "description is required"],
-  },
-  imageCover: {
-    type: String,
-    required: [true, "imageCover is required"],
-  },
-  images: [String],
-  startDates: {
-    type: [Date],
-    required: [true, "startDates is required"],
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select: false,
-  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+/*
+Virtual Properties: 
+enables separation between business logic and application logic
+derivative fields not to be persisted in the database
+eg conversion from mph to kph or days to weeks
+
+cannot be used in queries eg Tour.find( $where: { durationWeeks: 1 })
+*/
+tourSchema.virtual("durationWeeks").get(function () {
+  return Math.ceil(this.duration / 7);
 });
 
 const Tour = new mongoose.model("Tour", tourSchema);
