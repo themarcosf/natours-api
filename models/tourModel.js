0f2f3b876d2a39beebda0cd0423f6ebc0559c8e9
Marcos Florencio <marcosaurelio.adm@gmail.com>
2023-01-20 12:59:06 -0300

    prevent duplicate reviews

--- models/tourModel.js
+++ models/tourModel.js
@@ -83,6 +83,9 @@ const tourSchema = new mongoose.Schema(
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, "Rating must be above or equat to 1.0"],
+      max: [5, "Rating must be below or equal to 5.0"],
+      set: (val) => Math.round(val * 10) / 10,
     },
     ratingsQuantity: {
       type: Number,
