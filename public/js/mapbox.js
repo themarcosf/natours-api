cb6e366eb28adae0b83cbb291608565fd3a4b8a7
Marcos Florencio <marcosaurelio.adm@gmail.com>
2023-01-25 12:38:09 -0300

    add mapbox.js file

---
+++ public/js/mapbox.js
@@ -0,0 +1,48 @@
+const _locations = JSON.parse(document.getElementById("map").dataset.locations);
+
+// mapboxgl.accessToken = process.env.MAP_TOKEN;
+mapboxgl.accessToken =
+  "pk.eyJ1IjoibWFyY29zZi1uYXRvdXJzIiwiYSI6ImNsZGJxczhrdTAzODUzcW9kMGwyYTM5dTMifQ.4_IR3gtJl4qPv3KvLWmIrA";
+
+const _map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/marcosf-natours/cldbr3wb8000801nsfayzzbdx",
+  scrollZoom: false,
+  interactive: false,
+});
+
+const _bounds = new mapboxgl.LngLatBounds();
+
+_locations.forEach((loc) => {
+  // create marker
+  const _el = document.createElement("div");
+  _el.className = "marker";
+
+  // add marker to map
+  new mapboxgl.Marker({
+    element: _el,
+    anchor: "bottom",
+  })
+    .setLngLat(loc.coordinates)
+    .addTo(_map);
+
+  // add popup to map
+  new mapboxgl.Popup({
+    offset: 30,
+  })
+    .setLngLat(loc.coordinates)
+    .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+    .addTo(_map);
+
+  // extend map bounds to include location
+  _bounds.extend(loc.coordinates);
+});
+
+_map.fitBounds(_bounds, {
+  padding: {
+    top: 200,
+    bottom: 150,
+    left: 300,
+    right: 300,
+  },
+});
